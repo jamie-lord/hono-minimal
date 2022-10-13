840b43e308b3f0e14fd41e56270c2037d435189f
Yusuke Wada <yusuke@kamawada.com>
2022-10-13 21:16:59 +0900

    test: use miniflare environment

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testEnvironment: 'wrangler',
+  testEnvironment: 'miniflare',
   testMatch: ['**/test/**/*.+(ts|tsx)', '**/src/**/(*.)+(spec|test).+(ts|tsx)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'esbuild-jest',
