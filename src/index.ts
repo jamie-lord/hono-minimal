fd297c4dcca801f989795c2c122e86e772dbaf67
Jamie Lord <jamie@lord.technology>
2024-03-12 09:56:26 +0000

    Added other packages

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import { Hono } from 'hono'
-import { poweredBy } from 'hono/powered-by'
+import { Hono } from "hono";
+import { poweredBy } from "hono/powered-by";
+import jwt from "@tsndr/cloudflare-worker-jwt";
+import { v4 as uuidv4 } from "uuid";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('*', poweredBy())
+app.use("*", poweredBy());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  const id = uuidv4();
+  const test = await jwt.sign({ jti: id }, "test");
+  return c.text(`Hello Hono! ${test} ${id}`);
+});
 
-export default app
+export default app;
